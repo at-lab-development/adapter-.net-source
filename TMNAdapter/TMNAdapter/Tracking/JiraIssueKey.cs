93ef1cf8fe306979bdc09790916d9301d3223aa6
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-05-07 00:10:03 +0300

    EPMFARMATS-2455: Implemented JiraIssueKeyAttribute

--- TMNAdapter/TMNAdapter/Tracking/JiraIssueKey.cs
+++ TMNAdapter/TMNAdapter/Tracking/JiraIssueKey.cs
@@ -10,6 +10,17 @@ namespace TMNAdapter.Tracking
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     public class JiraIssueKeyAttribute : Attribute
     {
+        public string Key { get; }
+        public bool Disabled { get; }
+        public int RetryCountOnFailure { get; }
+        public bool DisableScreenshotOnFailure { get; }
 
+        public JiraIssueKeyAttribute(string key, bool disabled = false, int retryCountOnFailure = 1, bool disableScreenshotOnFailure = false)
+        {
+            Key = key;
+            Disabled = disabled;
+            RetryCountOnFailure = retryCountOnFailure;
+            DisableScreenshotOnFailure = disableScreenshotOnFailure;
+        }
     }
 }
