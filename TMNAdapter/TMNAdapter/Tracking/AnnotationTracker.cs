6d500e9a86155d52f106a9355054c8fccbb8c7c3
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-05-07 13:27:03 +0300

    EPMFARMATS-2455: Added attribute access from calling method using StackTrace

--- TMNAdapter/TMNAdapter/Tracking/AnnotationTracker.cs
+++ TMNAdapter/TMNAdapter/Tracking/AnnotationTracker.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TMNAdapter.Tracking
 {
-    class AnnotationTracker
+    public class AnnotationTracker
     {
+        public static T GetAttributeInCallStack<T>()
+        {
+            var stackTrace = new StackTrace();
+            T attribute = default(T);
 
+            for (var i = 1; i < stackTrace.FrameCount; i++)
+            {
+                var frame = new StackFrame(i);
+                object attributeObject = frame.GetMethod().GetCustomAttributes(true).FirstOrDefault(x => x.GetType() == typeof(T));
+
+                if (attributeObject != null)
+                {
+                    attribute = (T) attributeObject;
+                    break;
+                }
+            }            
+
+            return attribute;
+        }
     }
 }
