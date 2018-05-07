196ea32b35a3e7023a65181db946599643a9d935
Pavel_Irher <Pavel_Irher@EPBYMINW6607>
2018-05-07 09:58:14 +0300

    FileUtils class with blank methods added

---
+++ TMNAdapter/TMNAdapter/Utilities/FileUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMNAdapter.Utilities
+{
+    public class FileUtils
+    {
+        private static string TARGET_DIR = "\\target\\"; 
+        private readonly static string ATTACHMENTS_DIR = TARGET_DIR + "attachments\\";
+
+        public static string Save()
+        {
+            return null;
+        }
+
+        private static void WriteStackTrace()
+        {
+
+        }
+
+        public static string SaveFile()
+        {
+            return null;
+        }
+
+        public static void WriteXml()
+        {
+
+        }
+
+        static string GetTargetDir() => TARGET_DIR;
+        public static string GetAttachmentsDir() => ATTACHMENTS_DIR;
+    }
+}
