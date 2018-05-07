819ea9cce07dc82bbeab3ad9f2011f24cd11ae5d
Nastassia Hancharyk <Nastassia_Hancharyk@epam.com>
2018-05-07 14:09:33 +0300

    2454: some format corrections

--- TMNAdapter/TMNAdapter/Tracking/JiraInfoProvider.cs
+++ TMNAdapter/TMNAdapter/Tracking/JiraInfoProvider.cs
@@ -9,11 +9,11 @@ namespace TMNAdapter.Utilities
 {
   public class JiraInfoProvider
   {
-    private static Dictionary<string, List<TestParameters>> JIRAKeyParameters = new Dictionary<string, List<TestParameters>>();
-    private static Dictionary<string, List<string>> JIRAKeyAttachments = new Dictionary<string, List<string>>();
+    private static Dictionary<string, List<TestParameters>> jiraKeyParameters = new Dictionary<string, List<TestParameters>>();
+    private static Dictionary<string, List<string>> jiraKeyAttachments = new Dictionary<string, List<string>>();
 
     //TODO: to write methods SaveFile() and FindJIRATestKey();
-    private static string FindJIRATestKey()
+    private static string FindJiraTestKey()
     {
       //TODO: to write realization
       return null;
@@ -62,25 +62,25 @@ namespace TMNAdapter.Utilities
 
     public static void CleanFor(string issueKey)
     {
-      if (JIRAKeyParameters.ContainsKey(issueKey))
+      if (jiraKeyParameters.ContainsKey(issueKey))
       {
-        JIRAKeyParameters.Remove(issueKey);
+        jiraKeyParameters.Remove(issueKey);
       }
-      if (JIRAKeyAttachments.ContainsKey(issueKey))
+      if (jiraKeyAttachments.ContainsKey(issueKey))
       {
-        JIRAKeyAttachments.Remove(issueKey);
+        jiraKeyAttachments.Remove(issueKey);
       }
     }
 
     //Rewrite TestParameters
     //public static List<TestParameters> GetIssueParameters(string issueKey)
     //{
-    //  return JIRAKeyParameters[issueKey] ?? null;// don't see any sence
+    //  return jiraKeyParameters[issueKey] ?? null;// don't see any sence
     //}
 
     public static List<string> GetIssueAttachments(string issueKey)
     {
-      return JIRAKeyAttachments[issueKey] ?? null;// don't see any sence
+      return jiraKeyAttachments[issueKey] ?? null;// don't see any sence
     }
   }
 }
