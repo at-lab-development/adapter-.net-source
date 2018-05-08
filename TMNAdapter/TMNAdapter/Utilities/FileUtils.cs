c1ed2ec9bca0cac4dfc6af18bac03f381c2a64ff
Pavel_Irher <Pavel_Irher@EPBYMINW6607>
2018-05-08 14:06:21 +0300

    Comments modified, imports optimized.

--- TMNAdapter/TMNAdapter/Utilities/FileUtils.cs
+++ TMNAdapter/TMNAdapter/Utilities/FileUtils.cs
@@ -1,23 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 using TMNAdapter.Entities;
 
 namespace TMNAdapter.Utilities
 {
-	public class FileUtils
+    // FileUtils is a util class which provides useful methods for file writing
+    public class FileUtils
 	{
 		private readonly static string TARGET_DIR = "\\target\\";
-		private readonly static string ATTACHMENTS_DIR = TARGET_DIR + "attachments\\";
-		
+		private readonly static string ATTACHMENTS_DIR = TARGET_DIR + "attachments\\";		
 		  
-		 //Generate name of file with unique exception stack trace
-			
-		/// <returns> Returns exception message  </returns>
+		// Generate name of file with unique exception stack trace			
+		/// <returns> Returns exception message </returns>
 		public static string GetExceptionMessage(Exception ex)
 		{
 			string message = string.Empty;
@@ -34,13 +29,10 @@ namespace TMNAdapter.Utilities
 			return message;
 		}
 
-	 /*
-     * Writes stack trace in temporary file and save it to attachments directory
-     * Exception ex The exception for getting stacktrace
-     * filePath The path for output file
-     */
-
-		private static void WriteStackTrace(Exception ex, string filePath)
+        // Writes stack trace in temporary file and save it to attachments directory
+        ///<exception cref="Exception"> The exception ex for getting stacktrace </exception>  
+        ///<param name="filePath"> The path for output file </param> 
+        private static void WriteStackTrace(Exception ex, string filePath)
 		{
 			try
 			{
@@ -57,13 +49,13 @@ namespace TMNAdapter.Utilities
 			}
 		}
 
-        /// Copy and save file to the attachments default directory.If file in the
-        /// default attachments directory already exists the file will be created in
-        /// child directory with name contains current time in milliseconds using
-        /// TimeInMillis method.
-        /// <param name="file">the file to save</param>  
-        /// <param name="newFilePath">the path relative to attachments dir</param>  
-        ///  <returns>the path where file was actually saved </returns> 
+        // Copy and save file to the attachments default directory.If file in the
+        // default attachments directory already exists the file will be created in
+        // child directory with name contains current time in milliseconds using
+        // TimeInMillis method.
+        /// <param name="file"> The file to save </param>  
+        /// <param name="newFilePath"> The path relative to attachments dir </param>  
+        ///  <returns> The path where file was actually saved </returns> 
         public static string SaveFile(FileInfo file, string newFilePath)
         {
             try
@@ -86,10 +78,10 @@ namespace TMNAdapter.Utilities
             }
         }
 
-        /// Parse xml file using XmlSerializer. The entities for serialization are the same as in
-        /// Test Management Jira plugin.
-        /// <param name="result"> the list of issues for writing</param>
-        /// <param name="filePath">the path to output file</param>
+        // Parse xml file using XmlSerializer. The entities for serialization are the same as in
+        // Test Management Jira plugin.
+        /// <param name="result"> The list of issues for writing </param>
+        /// <param name="filePath"> The path to output file </param>
         public static void WriteXml(TestResult result, String filePath)
         {
             XmlSerializer formatter = new XmlSerializer(typeof(TestResult));
