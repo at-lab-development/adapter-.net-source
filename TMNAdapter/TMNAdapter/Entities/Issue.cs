ae7de6ac2aae246bbdf7f2d53ac795d1cffe0f6d
Aliaksandr Horski <Aliaksandr_Horski@epam.com>
2018-05-10 17:01:29 +0300

    update ExecutionTracker

--- TMNAdapter/TMNAdapter/Entities/Issue.cs
+++ TMNAdapter/TMNAdapter/Entities/Issue.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace TMNAdapter.Entities
 {
-    class Issue
-    {
-    }
+	[Serializable, XmlRoot("test")]
+	public class Issue
+	{
+		[XmlElement("key")]
+		public string IssueKey { get; set; }
+
+		[XmlElement("status")]
+		public string Status { get; set; }
+
+		[XmlElement("summary")]
+		public string Summary { get; set; }
+
+		[XmlElement("time")]
+		public string Time { get; set; }
+
+		[XmlArray("attachments"), XmlArrayItem("attachment")]
+		public List<String> Attachments { get; set; }
+
+		[XmlArray("parameters"), XmlArrayItem("parameter")]
+		public List<TestParameters> Parameters { get; set; }
+
+		public Issue(string issueKey, Status status)
+		{
+			IssueKey = issueKey;
+			Status = status.ToString();
+		}
+
+		public Issue(string issueKey, Status status, string time)
+		{
+			IssueKey = issueKey;
+			Status = status.ToString();
+			Time = time;
+		}
+
+
+	}
 }
