d38ec3c7fd79a00aefab005e8771beff75548e19
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-05-21 12:34:18 +0300

    EPMFARMATS-2486: Added test execution time formatting in ExecutionTracker

--- TMNAdapter/TMNAdapter/MSTest/ExecutionTracker.cs
+++ TMNAdapter/TMNAdapter/MSTest/ExecutionTracker.cs
@@ -77,13 +77,19 @@ namespace TMNAdapter.MSTest
                     IssueKey = issueModel.Key,
                     Summary = issueModel.Summary,
                     Status = issueModel.Status.ToString(),
-                    Time = issueModel.Time.ToString(),
+                    Time = FormatTime(issueModel.Time),
                     Attachments = issueModel.AttachmentFilePaths,
                     Parameters = issueModel.Parameters
-                });
+                });                
             }
 
             FileUtils.WriteXml(testResult, "tm-testng.xml");
+        }
+
+        private static string FormatTime(long? timeInMilliseconds)
+        {
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(Convert.ToDouble(timeInMilliseconds));
+            return $"{timeSpan.Minutes:D2}m:{timeSpan.Seconds:D2}s:{timeSpan.Milliseconds:D3}ms";
         }
     }
 }
