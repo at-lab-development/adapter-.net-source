df7ffd6140150417f04c83cd7919aa17c43699c0
Pavel_Irher <Pavel_Irher@EPBYMINW6607>
2018-05-21 12:03:26 +0300

    Screenshoter refactored

--- TMNAdapter/TMNAdapter/Common/Screenshoter.cs
+++ TMNAdapter/TMNAdapter/Common/Screenshoter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
+using TMNAdapter.Common.Models;
 using TMNAdapter.Tracking;
 using TMNAdapter.Utilities;
 
@@ -23,27 +25,28 @@ namespace TMNAdapter.Common
             return driverInstance != null;
         }
 
-        public static string TakeScreenshot()
+        public static void TakeScreenshot()
         {
-            if (!IsInitialized()) return null;
+            if (!IsInitialized()) return;
 
             if (!(driverInstance is RemoteWebDriver))
             {
                 Console.WriteLine("Unsupported driver type: " + driverInstance.GetType());
-                return null;
+                return;
             }
 
-            var data = DateTime.UtcNow.ToString().Replace(":", "-").Replace("/", ".");
-            string screenshotName = data + ".Jpeg";
-            string fullScreenshotPath = TestContext.CurrentContext.WorkDirectory + FileUtils.GetAttachmentsDir() + "\\" + screenshotName;
+            string screenshotName = $"screenshot_{DateTime.Now:yyyy-MM-ddTHH-mm-ss.fff}.jpeg";
+            string relativeScreenshotPath = FileUtils.GetAttachmentsDir() + "\\" + screenshotName;
+            string fullScreenshotPath = TestContext.CurrentContext.WorkDirectory + relativeScreenshotPath;
             var screenshot = ((ITakesScreenshot)driverInstance).GetScreenshot();
             screenshot.SaveAsFile(fullScreenshotPath, ScreenshotImageFormat.Jpeg);
-            FileInfo screen = new FileInfo(fullScreenshotPath);
 
-            JiraInfoProvider.SaveAttachment(screen);
-
-
-            return fullScreenshotPath != null ? screenshotName : null;
+            string issueKey = JiraInfoProvider.GetJiraTestKey();
+            IssueManager.AddIssue(new IssueModel()
+            {
+                Key = issueKey,
+                AttachmentFilePaths = new List<string>() { relativeScreenshotPath }
+            });
         }
     }
 }
