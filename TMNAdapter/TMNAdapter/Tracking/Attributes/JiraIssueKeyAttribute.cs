f79aae8ad0927e9d006515dcff0b1cc0a73cf6b3
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-05-22 14:30:25 +0300

    EPMFARMATS-2486: Removed unused properties in JiraIssueKeyAttribute

--- TMNAdapter/TMNAdapter/Tracking/Attributes/JiraIssueKeyAttribute.cs
+++ TMNAdapter/TMNAdapter/Tracking/Attributes/JiraIssueKeyAttribute.cs
@@ -18,21 +18,6 @@ namespace TMNAdapter.Tracking.Attributes
         /// </summary>
         public string Key { get; }
 
-        /// <summary>
-        /// Indicates whether <see cref="JiraIssueKeyAttribute"/> is applied 
-        /// </summary>
-        public bool Disabled { get; }
-
-        /// <summary>
-        /// Specifies the count of attempts to re-run test method on failure
-        /// </summary>
-        public int RetryCountOnFailure { get; }
-
-        /// <summary>
-        /// Indicates whether screenshots are allowed
-        /// </summary>
-        public bool DisableScreenshotOnFailure { get; }
-
         public ActionTargets Targets => ActionTargets.Test;
 
         private Stopwatch _stopWatch;
@@ -41,18 +26,11 @@ namespace TMNAdapter.Tracking.Attributes
         /// Initializes a new instance of <see cref="JiraIssueKeyAttribute"/>
         /// </summary>
         /// <param name="key">JIRA issue key</param>
-        /// <param name="disabled">Indicates whether <see cref="JiraIssueKeyAttribute"/> is applied</param>
-        /// <param name="retryCountOnFailure">Specifies the count of attempts to re-run test method on failure</param>
-        /// <param name="disableScreenshotOnFailure">Indicates whether screenshots are allowed</param>
-        public JiraIssueKeyAttribute(string key, bool disabled = false, int retryCountOnFailure = 1, bool disableScreenshotOnFailure = false)
+        public JiraIssueKeyAttribute(string key)
         {
             ValidationHelper.MatchPattern(key, nameof(key), @"((?<!([A-Za-z]{1,10})-?)[A-Z]+-\d+)");
-            ValidationHelper.InRange(retryCountOnFailure, nameof(retryCountOnFailure), 1, 5);
 
             Key = key;
-            Disabled = disabled;
-            RetryCountOnFailure = retryCountOnFailure;
-            DisableScreenshotOnFailure = disableScreenshotOnFailure;
         }
 
         public void BeforeTest(ITest test)
