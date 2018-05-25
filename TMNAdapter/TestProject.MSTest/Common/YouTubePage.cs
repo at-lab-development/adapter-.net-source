25f6b5dd7a93f9ca88b19b529ed54aad333828d1
Yauheniya_Strelkova <Yauheniya_Strelkova@epam.com>
2018-05-25 15:21:41 +0300

    added Browser and YouTubePage

---
+++ TMNAdapter/TestProject.MSTest/Common/YouTubePage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+
+namespace TestProject.Common
+{
+    public class YouTubePage
+    {
+        private static readonly By videoTitleLocator = By.ClassName("title");
+        private static readonly By authorNameLocator = By.XPath("//*[@id = 'owner-name']//*");
+        
+        protected Browser CurrentBrowser;
+
+        public YouTubePage(string url)
+        {
+            CurrentBrowser = Browser.Instance;
+            CurrentBrowser.WindowMaximize();
+            CurrentBrowser.NavigateTo(url);
+            Browser.Instance.WaitForPageLoaded();
+        }
+
+        public string GetVideoTitle()
+        {
+            Browser.Instance.WaitForElementIsVisible(videoTitleLocator);
+            return Browser.Instance.FindElement(videoTitleLocator).Text;
+        }
+        public string GetAuthorName()
+        {
+            Browser.Instance.WaitForElementIsVisible(authorNameLocator);
+            return Browser.Instance.FindElement(authorNameLocator).Text;
+        }
+    }
+}
---
+++ TMNAdapter/TestProject.MSTest/Tests/MS_YouTubeTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestProject.MSTest.Tests
+{
+    [TestClass]
+    public class YouTubeTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Screenshoter.Initialize(Browser.Instance.GetDriver());
+        }
+
+        [TestMethod]
+        //[JiraIssueKey("EPMFARMATS-2466")]
+        public void AlwaysPassedTest()
+        {
+            YouTubePage page = new YouTubePage("https://www.youtube.com/watch?v=UKKYpdWPSL8");
+            string author = page.GetAuthorName();
+
+            //JiraInfoProvider.SaveParameter("Author", author);
+            //JiraInfoProvider.SaveParameter("Title", page.GetVideoTitle());
+
+            Assert.AreEqual("author", "EPAM Systems Global");
+        }
+
+        [TestMethod]
+        //[JiraIssueKey("EPMFARMATS-2470")]
+        public void AlwaysFailedTest()
+        {
+            YouTubePage page = new YouTubePage("https://www.youtube.com/watch?v=sU4i4DTr1HQ");
+            string author = page.GetAuthorName();
+            string title = page.GetVideoTitle();
+
+            //JiraInfoProvider.SaveParameter("Author", author);
+            //JiraInfoProvider.SaveParameter("Title", title);
+
+            //Screenshoter.TakeScreenshot();
+
+            Assert.AreEqual("Atlassian", author);
+        }
+
+        [TestCleanup]
+        public void Close()
+        {
+            //Screenshoter.Initialize(null);
+            Browser.Instance.Quit();
+        }
+    }
+}
+
+
