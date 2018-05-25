3634f9246a2d45df4b2b0185629f8a3b035ff4ee
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-05-25 14:21:15 +0300

    EPMFARMATS-2935: Added TestProject.MSTest to test attaching files to .trx result files

---
+++ TMNAdapter/TestProject.MSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject.MSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject.MSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("711d9cf0-a366-4c97-9d40-8501bad7502d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TMNAdapter/TestProject.MSTest/Tests/BasicTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TMNAdapter.Tracking;
+
+namespace TestProject.MSTest
+{
+    [TestClass]
+    public class BasicTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        //[JiraIssueKey("EPMFARMATS-2447")]
+        public void CheckArtifacts()
+        {
+            var random = new Random();
+
+            JiraInfoProvider.SaveParameter("Random number", Convert.ToString(random.Next()));
+            JiraInfoProvider.SaveParameter("Random boolean", Convert.ToString(random.Next(0, 1)));
+            JiraInfoProvider.SaveParameter("Some static string", "Hello, world!");
+
+            JiraInfoProvider.SaveAttachment(new FileInfo($@"{AppDomain.CurrentDomain.BaseDirectory}..\..\Resources\jenkins-oops.jpg"));
+            JiraInfoProvider.SaveAttachment(new FileInfo($@"{AppDomain.CurrentDomain.BaseDirectory}..\..\Resources\jenkins-oops.jpg"));
+
+            TestContext.AddResultFile("");
+
+            Assert.Fail("Testing failed test behavior");
+        }
+
+        [TestMethod]
+        //[JiraIssueKey("EPMFARMATS-2471")]
+        public void UntestedTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
