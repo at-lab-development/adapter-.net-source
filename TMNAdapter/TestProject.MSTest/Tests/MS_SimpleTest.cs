d4f0a8547892e53716033038c32a416cc9e391e8
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-06-04 15:43:19 +0300

    EPMFARMATS-2935: Added test results processing for MSTest tests using new custom attribute

--- TMNAdapter/TestProject.MSTest/Tests/MS_SimpleTest.cs
+++ TMNAdapter/TestProject.MSTest/Tests/MS_SimpleTest.cs
@@ -1,37 +1,33 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using TMNAdapter.MSTest.Tracking.Attributes;
 
 namespace TestProject.MSTest.Tests
 {
     [TestClass]
     public class MS_SimpleTest : BaseTest
     {
-
-        [TestMethod]
-        //[JiraIssueKey("EPMFARMATS-2464")]
+        [JiraTestMethod("EPMFARMATS-2464")]
         public void TestParametersSetting()
         {
-            //JiraInfoProvider.SaveParameter("Name", "John");
-            //JiraInfoProvider.SaveParameter("Surname", "Smith");
-            //JiraInfoProvider.SaveParameter("Email", "Mr_Smith@gmail.com");
+            JiraInfoProvider.SaveParameter("Name", "John");
+            JiraInfoProvider.SaveParameter("Surname", "Smith");
+            JiraInfoProvider.SaveParameter("Email", "Mr_Smith@gmail.com");
 
             Assert.IsTrue(true);
         }
 
-        [TestMethod]
-        //[JiraIssueKey("EPMFARMATS-2472")]
+        [JiraTestMethod("EPMFARMATS-2472")]
         public void TestExeptionInTest_1()
         {
             throw new Exception("Testing test with exception");
         }
 
-        [TestMethod]
-        //[JiraIssueKey("EPMFARMATS-2472")]
+        [JiraTestMethod("EPMFARMATS-2472")]
         public void TestExeptionInTest_2()
         {
-            //JiraInfoProvider.SaveParameter("Value4", "Sample4");
+            JiraInfoProvider.SaveParameter("Value4", "Sample4");
             throw new Exception("Testing test with exception");
         }
     }
 }
-
