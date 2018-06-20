78ca84ccba2608cf2064e006dd05e36433f550a7
Liudmila Astrouskaya <liudmila_astrouskaya@epam.com>
2018-06-20 11:44:09 +0300

    Deleted unnecessary references

--- TMNAdapter/TMNAdapter.NUnit/Utilities/Screenshoter.cs
+++ TMNAdapter/TMNAdapter.NUnit/Utilities/Screenshoter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
-using TMNAdapter.Core.Common;
-using TMNAdapter.Core.Common.Models;
-using TMNAdapter.Core.Tracking.Attributes;
+﻿using TMNAdapter.Core.Tracking.Attributes;
 using TMNAdapter.Core.Utilities;
 using TMNAdapter.Tracking.Attributes;
 
