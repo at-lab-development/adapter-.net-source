5de16bcc9187f68e2bb5effbb416f4b376425280
Pavel_Irher <Pavel_Irher@EPBYMINW6607>
2018-07-26 14:46:45 +0300

    result file name typo fixed

--- TMNAdapter/TMNAdapter.Core/Common/Constants.cs
+++ TMNAdapter/TMNAdapter.Core/Common/Constants.cs
@@ -4,6 +4,6 @@
     {
         internal const string TARGET_DIR = "\\target";
         internal const string ATTACHMENTS_DIR = TARGET_DIR + "\\attachments";
-        internal const string RESULT_FILE_NAME = "jira_tm_report.xml";
+        internal const string RESULT_FILE_NAME = "jira-tm-report.xml";
     }
 }
